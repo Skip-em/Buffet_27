fff4c387577cbaa669fa70be5a6f313a17358801
Skip-em <Microraptor23@outlook.com>
2026-09-04 18:03:14 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,18 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		// Variables:
+
+		String b = new String("Happy Birthday to you");
+		String d = new String("Happy Birthday dear ");
+		String name = new String("Jims");
+
+		//Print
+
+		System.out.println(b); 
+		System.out.println(b); 
+		System.out.println(d+name); 
+		System.out.println(b);
 	}
 }
